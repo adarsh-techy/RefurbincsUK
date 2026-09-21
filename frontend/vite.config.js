3e22db8e7ab5a34c53a67626638927069daac49c
Adarsh <adarsh@Adarshs-MacBook-Pro.local>
2026-09-22 01:49:34 +0530

    Update vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
@@ -8,7 +7,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react()],
   envDir: path.resolve(__dirname, '..'),
   server: {
     host: true,
